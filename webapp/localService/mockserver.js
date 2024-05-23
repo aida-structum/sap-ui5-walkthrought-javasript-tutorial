ef5891074f9d6c476c0b36ca9f9274dbcce63710
unknown <iamaidamina@gmail.com>
2024-05-23 10:52:19 -0500

    step-26 Mock Server Configuration

---
+++ webapp/localService/mockserver.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+  "sap/ui/core/util/MockServer",
+  "sap/base/util/UriParameters"
+], (MockServer, UriParameters) => {
+	"use strict";
+
+	return {
+		init: function() {
+			// create
+			var oMockServer = new MockServer({
+				rootUri: "http://localhost:8081/V2/Northwind/Northwind.svc/"
+			});
+
+			var oUriParameters= new UriParameters(window.location.href);
+
+			// configure mock server with a delay
+			MockServer.config({
+				autoRespond: true,
+				autoRespondAfter: oUriParameters.get("serverDelay") || 500
+			});
+
+			// simulate
+			var sPath = sap.ui.require.toUrl("sap-ui5-walkthrough/localService");
+			
+      oMockServer.simulate(sPath+"/metadata.xml", sPath+"/mockdata");
+			// start
+			oMockServer.start();
+		}
+	};
+});
